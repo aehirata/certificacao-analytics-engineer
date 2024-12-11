db8a5c807c4b34776ad0d9b54a25b88e6c68485d
Amanda Hirata <amanda.hirata@indicium.tech>
2024-12-11 00:33:45 +0000

    customer

--- models/mart/dim_customer.sql
+++ models/mart/dim_customer.sql
@@ -1,5 +1,4 @@
 with
-
     businessentitycontact as (
         select *
         from {{ ref('stg_adventure_works__businessentitycontact') }}
@@ -15,18 +14,48 @@ with
         from {{ ref('stg_adventure_works__person') }}
     )
 
-    , joined as (
+    , store as (
+        select *
+        from {{ ref('stg_adventure_works__store') }}
+    )
+
+    , person_customer as (
         select
-            customer.customerid
-            , customer.personid
-            , person.firstname
-            , person.middlename
-            , person.lastname
-            , person.firstname || ' ' || coalesce(person.middlename, '') || ' ' || person.lastname as fullname
+            customer.personid as businessentityid
+            , person.firstname || ' ' || coalesce(person.middlename, '') || ' ' || person.lastname as customer_name
+            , 'Person' as customer_type
         from businessentitycontact
-        inner join person using (businessentityid)
-        inner join customer using (customerid)
+        inner join person 
+            on person.businessentityid = businessentitycontact.personid
+        inner join customer
+            on customer.personid = person.businessentityid
+    )
+
+    , store_customer as (
+        select
+            businessentityid
+            , name as customer_name
+            , 'Store' as customer_type
+        from store
+        inner join customer
+            on store.businessentityid = customer.storeid
+    )
+
+    , all_customers as (
+        select *
+        from person_customer
+        union
+        select *
+        from store_customer
+    )
+
+    , generate_sk as (
+        select 
+            {{ dbt_utils.generate_surrogate_key(['businessentityid']) }} as customer_pk
+            , customer_name
+            , customer_type
+        from all_customers
     )
 
 select *
-from joined
+from generate_sk
